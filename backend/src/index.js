c03b316127e02ebcc8b03585647ee6c8484b4454
jeel1802 <160886657+jeel1802@users.noreply.github.com>
2025-05-21 18:56:35 +0530

    push

--- backend/src/index.js
+++ backend/src/index.js
@@ -42,7 +42,7 @@ if(process.env.NODE_ENV === "production"){
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });a
+  });
 }
 
 // Start server
