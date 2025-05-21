3d73e033af91d34dd3e911d53501661a15152ac9
jeel1802 <160886657+jeel1802@users.noreply.github.com>
2025-05-21 19:24:03 +0530

    again new

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 export const HOST = import.meta.env.VITE_SERVER_URL;
 
-export const AUTH_ROUTES = "api/auth";
+export const AUTH_ROUTES = "auth";
 export const SIGNUP_ROUTES = `${AUTH_ROUTES}/signup`
 export const LOGIN_ROUTES = `${AUTH_ROUTES}/login`
 export const LOGOUT_ROUTES = `${AUTH_ROUTES}/logout`
