235d620a85d9a7efa6bad88bcae6cba59f8f9604
jeel1802 <160886657+jeel1802@users.noreply.github.com>
2025-06-09 14:21:54 +0530

    new update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,11 @@
-import { HOST } from "../utils/constants";
 import axios from "axios";
 
+const productionBaseURL = import.meta.env.VITE_SERVER_URL; // Set this in your .env file
+
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-    withCredentials : true
-})
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : `${productionBaseURL}/api`, // Correct backend URL in production
+  withCredentials: true,
+});
