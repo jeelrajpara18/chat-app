ca987a4d52a049fe69b1b1bc0d216466bf6bf759
jeel1802 <160886657+jeel1802@users.noreply.github.com>
2025-06-09 18:45:09 +0530

    dee

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,7 +7,7 @@ export const generateToken = (userId , res) => {
         maxAge : 7*24*60*60*1000,
         httpOnly : true,
         sameSite : "None",
-        secure : true
+        secure : process.env.NODE_ENV == "production"
     })
     return token
 }
