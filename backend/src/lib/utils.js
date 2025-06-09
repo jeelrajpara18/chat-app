338068e7ca3d7829bff528e6e02f0bea06824a13
jeel1802 <160886657+jeel1802@users.noreply.github.com>
2025-06-09 19:04:09 +0530

    ddf

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,13 +1,16 @@
 import jwt from "jsonwebtoken";
-export const generateToken = (userId , res) => {
-    const token = jwt.sign({userId} , process.env.JWT_SECRET_KEY , {
-        expiresIn : "7d"
-    })
-    // res.cookie("jwt" , token , {
-    //     maxAge : 7*24*60*60*1000,
-    //     httpOnly : true,
-    //     sameSite : "None",
-    //     secure : process.env.NODE_ENV == "development"
-    // })
-    return token
-}
+
+export const generateToken = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+        expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        httpOnly: true, // Prevent XSS attacks
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Cross-site cookies in prod
+        secure: process.env.NODE_ENV === "production", // HTTPS-only in prod
+    });
+
+    return token;
+};
